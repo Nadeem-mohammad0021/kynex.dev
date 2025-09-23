96b368749bbc109edf7746b936018e0bf6051580
Nadeem Mohammad <nadeemmohammad.0021@gmail.com>
2025-09-23 16:42:03 +0530

    Version 2

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://kynex.dev/',   // replace with your domain
+  generateRobotsTxt: true,        // also creates robots.txt
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
